f733dfb4ffe6b0afbba172d7046214f07eda01b4
Sayan Chakraborty <sayanchakraborty885@gmail.com>
2025-04-13 00:44:04 +0530

    dark mode added

--- Frontend/Task-App-Frontend/src/App.jsx
+++ Frontend/Task-App-Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
+import NavBar from "./components/NavBar";
 
 const App = () => {
     const [refresh, setRefresh] = useState(false);
@@ -12,14 +13,7 @@ const App = () => {
     return (
         <div>
             <div>
-                <header className="mb-10 text-center">
-                    <h1 className="text-4xl font-semibold tracking-tight text-gray-800 mb-2">
-                        Task Manager
-                    </h1>
-                    <p className="text-gray-400">
-                        Organize, Prioritize, Accomplish
-                    </p>
-                </header>
+                <NavBar/>
 
                 <TaskForm onTaskCreated={triggerRefresh} />
                 <TaskList refresh={refresh} />
