da8fc0d1de0a2762377ae996049fb6d27acd9ab0
Sauvik Chatterjee <sauvikchatterjee9@gmail.com>
2025-05-17 10:03:14 +0530

    CI CD testing

--- Frontend/Task-App-Frontend/src/App.jsx
+++ Frontend/Task-App-Frontend/src/App.jsx
@@ -3,29 +3,27 @@ import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
 
 const App = () => {
-    const [refresh, setRefresh] = useState(false);
+  const [refresh, setRefresh] = useState(false);
 
-    const triggerRefresh = () => {
-        setRefresh(!refresh);
-    };
+  const triggerRefresh = () => {
+    setRefresh(!refresh);
+  };
 
-    return (
-        <div>
-            <div>
-                <header className="mb-10 text-center">
-                    <h1 className="text-4xl font-semibold tracking-tight text-gray-800 mb-2">
-                        Task Manager
-                    </h1>
-                    <p className="text-gray-400">
-                        Organize, Prioritize, Accomplish
-                    </p>
-                </header>
+  return (
+    <div className="bg-amber-200">
+      <div>
+        <header className="mb-10 text-center">
+          <h1 className="text-4xl font-semibold tracking-tight text-gray-800 mb-2">
+            My Task Manager
+          </h1>
+          <p className="text-gray-400">Organize, Prioritize, Accomplish</p>
+        </header>
 
-                <TaskForm onTaskCreated={triggerRefresh} />
-                <TaskList refresh={refresh} />
-            </div>
-        </div>
-    );
+        <TaskForm onTaskCreated={triggerRefresh} />
+        <TaskList refresh={refresh} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
